19a75370878af36d0b05c4903a0137e045bd0f12
Ebenezer <akhonyaebenezer@gmail.com>
2023-07-02 19:32:44 +0300

    [Chore]: Add tests for interview round 1

--- 0x00-pascal_triangle/tests/test_triangles.py
+++ 0x00-pascal_triangle/tests/test_triangles.py
@@ -5,16 +5,31 @@ unittest for the interview question
 """
 
 from unittest import TestCase
-from  triangle import pascal_triangle
+import triangle
+
 
 class TestTriangle(TestCase):
     """
     Test suites for the function
     """
 
-    def test_documenation(self):
-        self.assertIsNotNone(pascal_triangle.__doc__)
+    def test_function_documenation(self):
+        """
+        Esure the function is well documented
+        """
+        self.assertIsNotNone(triangle.pascal_triangle.__doc__)
 
-    def test_inputs(self):
+    def test_module_documentation(self):
+        """
+        Esure the module is well documented
+        """
+        self.assertIsNotNone(triangle.__doc__)
+
+    def test_input_exceptions(self):
+        """
+        Esure the function raises the exception
+        """
         with self.assertRaises(TypeError):
-            pascal_triangle("taylorshow")
+            triangle.pascal_triangle("taylorshow")
+            triangle.pascal_triangle("Jane Doe")
+            triangle.pascal_triangle({"show": "blindspot"})
