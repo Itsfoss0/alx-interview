6df4e1ab433c81b1990d6f0753457f24cca314df
Itsfoss0 <akhonyaebenezer@gmail.com>
2023-07-14 01:52:15 +0300

    [Chore]: Round 3 - Min Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+
+"""
+Module for the   third round interview question
+Minimum number of operations
+"""
+
+
+def minOperations(n: int) -> int:
+    """
+    Determine the minimum opeartions to run
+    Args:
+        n (int):
+    Returns:
+        Returns an int
+    """
+    if not isinstance(n, int):
+        raise TypeError('Expected an integer')
+    if n < 2:
+        return 0
+    operations, root = 0, 2
+    while root <= n:
+        if n % root == 0:
+            operations += root
+            n = n / root
+            root -= 1
+        root += 1
+    return operations
