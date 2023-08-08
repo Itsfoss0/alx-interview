ec641b8e5fbd1a3d48f3d3861c13916224f88ecf
Ebenezer <akhonyaebenezer@gmail.com>
2023-08-08 20:25:50 +0300

    [Chore]: Round - 6 Starwars API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+
+/* eslint-disable */
+const request = require('request');
+
+function getCharacterEndPoints(filmID) {
+  const filmURL = `https://swapi-api.alx-tools.com/api/films/${filmID}`;
+  return new Promise((resolve, reject) => {
+    request(filmURL, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body).characters);
+      }
+    });
+  });
+}
+const starWarID = process.argv[2]
+getCharacterEndPoints(starWarID)
+  .then((userEndPoints) => {
+    const requests = userEndPoints.map((element) => {
+      return new Promise((resolve, reject) => {
+        request(element, (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(body).name);
+          }
+        });
+      });
+    });
+
+    Promise.all(requests)
+      .then((characterNames) => {
+        characterNames.forEach((name) => {
+          console.log(name);
+        });
+      })
+      .catch((error) => console.log(error));
+  })
+  .catch((error) => console.log(error));
