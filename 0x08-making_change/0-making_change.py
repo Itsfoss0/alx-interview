3afa51badf012cf79c2e3cad0d417422101ae536
Ebenezer <akhonyaebenezer@gmail.com>
2023-08-31 19:30:09 +0300

    [Chore]: Making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Making change
+"""
+
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
