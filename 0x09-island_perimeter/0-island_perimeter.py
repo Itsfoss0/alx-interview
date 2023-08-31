3f42640c970fbba6d27342bbaac209c8db9c626e
Ebenezer <akhonyaebenezer@gmail.com>
2023-08-31 19:12:46 +0300

    [Chore]: Add Island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,25 @@
+def island_perimeter(grid):
+    """Calculate the perimeter of the island in the grid.
+
+    Args:
+        grid (list[list[int]]): A 2D grid representing the island.
+
+    Returns:
+        int: The perimeter of the island.
+    """
+    num_rows = len(grid)
+    num_cols = len(grid[0])
+    perimeter = 0
+    for row in range(num_rows):
+        for col in range(num_cols):
+            if grid[row][col] == 1:
+                perimeter += 4  # Start with 4 sides
+
+                # Check left neighbor
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+
+                # Check above neighbor
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+    return perimeter
